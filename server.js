a61bef698e2ba50be684b3e157c597165beb99a1
praveen T <praveenvicky235@gmail.com>
2023-04-10 00:33:15 +0530

    Checkpoint-3

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/movies') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end('All Movies Data in JSON format from Mongo DB');
+  } else if (req.url === '/genres') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end('All Genres Data in JSON format from Mongo DB');
+  } else if (req.url === '/artists') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end('All Artists Data in JSON format from Mongo DB');
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('404 Not Found');
+  }
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
